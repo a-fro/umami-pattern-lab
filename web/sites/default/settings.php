ce625b3c17e0bda69f9bf38895f68ff5a9efc9fe
Aaron Froehlich <aaronfroehlich@gmail.com>
2018-04-12 09:47:38 -0500

    Removing db connection info

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -786,14 +786,4 @@ if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
 $config_directories['sync'] = '../config/sync';
-$databases['default']['default'] = array (
-  'database' => 'drupal8',
-  'username' => 'drupal8',
-  'password' => 'drupal8',
-  'prefix' => '',
-  'host' => 'database',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
 $settings['install_profile'] = 'demo_umami';
